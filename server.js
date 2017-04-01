26922ea7887b40f456a8c3a9610c02e2ba999ec4
pemo moore <pemo moore>
2017-03-31 20:47:37 -0400

    heroku 5 (thanks jarrett tolmann if it works)

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ require("./app/routes/s3.js")(app);
 
 
 // Syncing our sequelize models and then starting our express app
-// db.sequelize.sync({ force: false }).then(function() {
-// });
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+});
